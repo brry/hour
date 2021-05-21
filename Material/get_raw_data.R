073cac1734914a16846fd25b97bd2375bf690cd3
brry <berryboessenkool@hotmail.com>
2021-05-21 08:38:47 +0200

    update raw data code, incl. pdf slides update

--- Material/get_raw_data.R
+++ Material/get_raw_data.R
@@ -1,18 +1,18 @@
 
-# 1 hour R Intro code
+# 1-3 hour R Intro code
 # Berry Boessenkool, berry-b@gmx.de, 2018-06
 
 
 wd <- getwd()
 if(!grepl("Material", wd)) setwd("Material/")
 
-# Benötigtes Paket ----
+# needed package ----
 if(!requireNamespace("rdwd")) install.packages("rdwd")
 
 library(rdwd) # See intro at https://github.com/brry/rdwd#rdwd
 
 
-# Daten ----
+# Data ----
 link <- selectDWD("Potsdam", res="daily", var="kl", per="recent")
 file <- dataDWD(link, read=FALSE, force=NA, overwrite=TRUE)
 clim <- readDWD(file)
@@ -21,14 +21,19 @@ meta <- readVars(file)
 str(clim)
 
 
-# Daten zum spaeteren einlesen exportieren:
+# Export data for later reading ----
 
 meta$Label <- paste0(meta$Kurz, "  [", meta$Einheit, "]")
 write.table(clim, file="Rintro/clim.txt", sep="\t", quote=FALSE, row.names=FALSE)
 write.table(meta, file="Rintro/meta.txt", sep=";", quote=FALSE, row.names=FALSE, fileEncoding="utf-8")
 
 
-# ZIP Ordner erstellen:
+# Update PDF slides ----
+if(requireNamespace("knitPres"))  # https://github.com/brry/knitpres#knitpres
+  knitPres::knit_hand_pres("Rintro.Rnw")
+
+
+# Create ZIP folder ----
 
 file.copy("Rintro.pdf",      "Rintro", overwrite=TRUE)
 unlink("Rintro.zip")
